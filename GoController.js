4d5614bbcfb9e17a07ca2a08ad89d5f57fda1e1c
Kevin Nguyen <itskevinwin@gmail.com>
2020-04-28 18:45:13 -0700

    GoController added

---
+++ GoController.js
@@ -0,0 +1,16 @@
+// takes in selected filters and applies them to the CSV file and send back the results
+class GoController {
+
+    // returns image
+    updateDate = (fromSettings) => {
+
+    }
+
+    // after updating data, show the results onto the "Filtered" results view
+    showFilteredResults = () => {
+
+    }
+
+}
+
+export default GoController; 
