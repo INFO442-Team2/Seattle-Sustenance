101526a7bd2d2e607382f0fbf490c92c53bfb5c9
Rachel Vuu <rachelvuu@Rachels-MBP-3.hsd1.wa.comcast.net>
2020-04-28 19:20:39 -0700

    Added model stub

---
+++ seattle-sustenance/src/components/stubs/MealProgramsModel.js
@@ -0,0 +1,12 @@
+class MealProgramsModel {
+    constructor(data) {
+        this.data = data;
+    }
+
+    // use the user filters to apply to the csv
+    FilterResults ( userInputs ) {
+        // code to modify csv data based on filter selection
+        return filteredData;
+    }
+
+}
