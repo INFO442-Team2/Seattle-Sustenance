a00d2e3b3c82b155d16de728f41790057952b60a
kaitlyncameron <kcam21@uw.edu>
2020-06-01 22:03:39 -0700

    updated result style & about pic

--- app/src/components/stubs/FindAMealView.js
+++ app/src/components/stubs/FindAMealView.js
@@ -176,15 +176,15 @@ class FindAMealView extends Component {
                 {meals.map((card, i) => {
                 return (
                     <Card>
-                    <p>{meals[i].properties["Name_of_Program"]}</p>
-                    <p>{meals[i].properties["People_Served"]}</p>
-                    <p>{meals[i].properties["Meal_Served"]}</p>
-                    <p>
+                    <p className="program-name">{meals[i].properties["Name_of_Program"]}</p>
+                    <p className="people-served">{meals[i].properties["People_Served"]}</p>
+                    <p className="meal-served">{meals[i].properties["Meal_Served"]}</p>
+                    <p className="meal-time">
                         {meals[i].properties["Time_Start"]}-
                         {meals[i].properties["Time_End"]}
                     </p>
-                    <p>{meals[i].properties["Days"]}</p>
-                    <p>{meals[i].properties["Location"] + " " + meals[i].properties["Zipcode"]}</p>
+                    <p className="days-served">{meals[i].properties["Days"]}</p>
+                    <p className="location-served">{meals[i].properties["Location"] + " " + meals[i].properties["Zipcode"]}</p>
                     </Card>
                 );
                 })}
